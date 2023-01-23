58cd7d5aebe84f0ae454e5d4ffa3ffcd33044c90
MinKorea <77200358+MinKorea@users.noreply.github.com>
2023-01-22 22:44:36 -0500

    Add files via upload

---
+++ 03_Raytracing_Step6_PerspectiveView/Hit.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <iostream>
+#include <glm/glm.hpp>
+#include <glm/gtx/string_cast.hpp>
+
+namespace hlab
+{
+    class Object;
+
+    class Hit
+    {
+    public:
+        float d;          // distance of start position of ray to the hit position 
+        glm::vec3 point;  // Position of hit
+        glm::vec3 normal; // Normal vector of the position of hit 
+
+        std::shared_ptr<Object> obj; // 나중에 물체의 재질 등을 가져오기 위한 포인터
+    };
+}
